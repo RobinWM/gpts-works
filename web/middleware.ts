3dc31aad482c5c31a14b4651f7b8e74075924378
wangmeng <wangmeng@i72.com>
2024-03-08 18:04:13 +0800

    feat: update

--- web/middleware.ts
+++ web/middleware.ts
@@ -1,6 +1,6 @@
 import { authMiddleware } from '@clerk/nextjs'
 
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+// See:https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({})
 
 export const config = {
